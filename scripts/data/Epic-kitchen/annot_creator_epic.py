cc5c15208115f5f7637c92b5cad08d414c6f516f
mona <moohnaai@gmail.com>
2023-01-09 18:28:57 +0000

    modeified annot_creator_epic.py

--- scripts/data/Epic-kitchen/annot_creator_epic.py
+++ scripts/data/Epic-kitchen/annot_creator_epic.py
@@ -8,8 +8,8 @@ val_df = {'path':[], 'label_name':[], 'label_num':[]}
 
 # root addresses
 
-root_add_train = "/home/mona/VideoMAE/dataset/Epic_kitchen/annotation/EPIC_100_train.csv"
-root_add_val = "/home/mona/VideoMAE/dataset/Epic_kitchen/annotation/EPIC_100_validation.csv" 
+root_add_train = "/mnt/welles/scratch/datasets/Epic-kitchen/EPIC-KITCHENS/EPIC_100_train.csv"
+root_add_val = "/mnt/welles/scratch/datasets/Epic-kitchen/EPIC-KITCHENS/EPIC_100_validation.csv" 
 video_mp4_root_add_train = "/mnt/welles/scratch/datasets/Epic-kitchen/EPIC-KITCHENS/EPIC_100_action_recognition/mp4_videos/train"
 video_mp4_root_add_val = "/mnt/welles/scratch/datasets/Epic-kitchen/EPIC-KITCHENS/EPIC_100_action_recognition/mp4_videos/validation"
 
@@ -20,8 +20,10 @@ for i, item in train_label.iterrows():
     path = os.path.join(video_mp4_root_add_train, f"video_{i}.mp4")
     if not os.path.exists(path):
         continue
-    label_name = item ['verb']
-    label_num = item ['verb_class']
+    # label_name = item ['verb']
+    # label_num = item ['verb_class']
+    label_name = item ['noun']
+    label_num = item ['noun_class']
     train_df['path'].append(path)
     train_df['label_name'].append(label_name)
     train_df['label_num'].append(label_num)
@@ -30,11 +32,13 @@ for i, item in train_label.iterrows():
 val_label = pd.read_csv(root_add_val)
 for i, item in val_label.iterrows():
     id = i
-    path = os.path.join(video_mp4_root_add_val, f"video_{i}.MP4")
+    path = os.path.join(video_mp4_root_add_val, f"video_{i}.mp4")
     if not os.path.exists(path):
         continue
-    label_name = item ['verb']
-    label_num = item ['verb_class']
+    # label_name = item ['verb']
+    # label_num = item ['verb_class']
+    label_name = item ['noun']
+    label_num = item ['noun_class']
     val_df['path'].append(path)
     val_df['label_name'].append(label_name)
     val_df['label_num'].append(label_num)
@@ -45,7 +49,7 @@ val_df = pd.DataFrame(val_df)
 
 
 # to_csv() 
-csv_annotation_root = "/home/mona/VideoMAE/dataset/Epic_kitchen/annotation/verb"
+csv_annotation_root = "/home/mona/VideoMAE/dataset/Epic_kitchen/annotation/noun"
 if not os.path.exists(csv_annotation_root):
     os.makedirs(csv_annotation_root)
 train_df.to_csv(path_or_buf=os.path.join(csv_annotation_root, "train.csv"), sep=' ', na_rep='', float_format=None, 
